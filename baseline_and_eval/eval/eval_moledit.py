6c8d052a5117e387acc258957ec2d0a4b4e54318
riiiiiiin <109677966+riiiiiiin@users.noreply.github.com>
2025-12-10 03:01:09 +0000

    Fixed mol parsing error counting logic as RDKit may silently print error and returns None

--- baseline_and_eval/eval/eval_moledit.py
+++ baseline_and_eval/eval/eval_moledit.py
@@ -187,11 +187,23 @@ def mol_prop(mol, prop):
         raise ValueError(f'Property {prop} not supported')
 
 def is_valid_smiles(smiles):
+    '''
+    Chem.MolFromSmiles documentation (version 2025.9.3):
+    
+    RETURNS:
+        
+            a Mol object, None on failure.
+        
+           The optional replacements dict can be used to do string substitution of abbreviations 
+           in the input SMILES. The set of substitutions is repeatedly looped through until 
+           the string no longer changes. It is the responsibility of the caller to make sure 
+           that substitutions results in legal and sensible SMILES. 
+    '''
     try:
-        Chem.MolFromSmiles(smiles)
-        return True
+        return Chem.MolFromSmiles(smiles) is not None
     except:
         return False
+
 
 GROUP_SET={
     "benzene",
@@ -257,8 +269,7 @@ def check_edit_add_valid(src, tgt, group)->bool:
     try:
         assert is_valid_smiles(tgt), f"无效的目标分子SMILES: {tgt}"
     except Exception as e:
-        print(e)
-        return False
+        raise e
     if mol_prop(tgt, "num_"+group) == mol_prop(src, "num_"+group) + 1:
         return True
     else:
@@ -271,8 +282,7 @@ def check_edit_del_valid(src, tgt, group)->bool:
     try:
         assert is_valid_smiles(tgt), f"无效的目标分子SMILES: {tgt}"
     except Exception as e:
-        print(e)
-        return False
+        raise e
     return mol_prop(tgt, "num_"+group) == mol_prop(src, "num_"+group) - 1
 
 def check_edit_sub_valid(src, tgt, remove_group, add_group)->bool:
@@ -282,8 +292,7 @@ def check_edit_sub_valid(src, tgt, remove_group, add_group)->bool:
     try:
         assert is_valid_smiles(tgt), f"无效的目标分子SMILES: {tgt}"
     except Exception as e:
-        print(e)
-        return False
+        raise e
     return mol_prop(tgt, "num_"+remove_group) == mol_prop(src, "num_"+remove_group) - 1 and mol_prop(tgt, "num_"+add_group) == mol_prop(src, "num_"+add_group) + 1
 
 def calculate_molecular_similarity(mol1, mol2, fingerprint_type='Morgan', 
@@ -377,20 +386,25 @@ def eval_moledit_from_list(src_list, pred_list, group_a, group_b, task, total_nu
     #   total_number: len(gt_molecules)+len(cases that cannot extract SMILES)
     
     correct_num = 0
+    valid_num = 0
     for i in range(len(src_list)):
-        if task in ['add']:
-            if check_edit_add_valid(src=src_list[i], tgt=pred_list[i], group=group_a[i]):
-                correct_num += 1
-        if task in ['delete']:
-            if check_edit_del_valid(src=src_list[i], tgt=pred_list[i], group=group_a[i]):
-                correct_num += 1
-        if task == 'sub':
-            if check_edit_sub_valid(src=src_list[i], tgt=pred_list[i], remove_group=group_b[i], add_group=group_a[i]):
-                correct_num += 1
+        try:
+            if task in ['add']:
+                if check_edit_add_valid(src=src_list[i], tgt=pred_list[i], group=group_a[i]):
+                    correct_num += 1
+            if task in ['delete']:
+                if check_edit_del_valid(src=src_list[i], tgt=pred_list[i], group=group_a[i]):
+                    correct_num += 1
+            if task == 'sub':
+                if check_edit_sub_valid(src=src_list[i], tgt=pred_list[i], remove_group=group_b[i], add_group=group_a[i]):
+                    correct_num += 1
+            valid_num += 1
+        except Exception as e:
+            print(e)
     
     my_dict = {
         "correct_rate": correct_num / total_number,
-        f"{task}-valid-rate": len(pred_list) / total_number,
+        f"{task}-valid-rate": valid_num / total_number,
     }
     return my_dict
     
